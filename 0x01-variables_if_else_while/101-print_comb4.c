b8f141bc7b466676bda70955f8965a9e9bd358a7
villeantropist <villeantropist@gmail.com>
2022-12-12 07:47:33 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,8 +10,8 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 	int hundreds = '0';
-
-	for (hundreds = '0'; hundreds <= '0'; hundreds++)
+	
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
