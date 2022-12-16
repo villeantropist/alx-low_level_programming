adbaadfc849757f9533aa7050c3bc46c77c01d5f
villeantropist <villeantropist@gmail.com>
2022-12-16 13:12:49 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,6 @@
 
 int _isupper(int c)
 {
-<<<<<<< HEAD
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-=======
     if (c >= 'A' && c <= 'Z')
     {
         return (1);
@@ -27,4 +16,3 @@ int _isupper(int c)
         return (0);
     }
 }
->>>>>>> 7f19bc34c5daf3a0db59eb74b91c07c5600462f3
