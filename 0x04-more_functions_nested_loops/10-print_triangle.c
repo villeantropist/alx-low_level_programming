7588da0f4946c3615b2af517ae76fd3f9a46c976
villeantropist <villeantropist@gmail.com>
2022-12-17 01:07:22 +0100

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@
 void print_triangle(int size)
 {
 	int i = 0, j, n = size - 1;
-	
+
 	if (size > 0)
 	{
 		for (; i < size; i++)
@@ -27,4 +27,3 @@ void print_triangle(int size)
 	else
 		_putchar('\n')
 }
-
