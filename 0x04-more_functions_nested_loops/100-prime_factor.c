8ab4bde367896c2b67869f637d81610f196c9f2e
villeantropist <villeantropist@gmail.com>
2022-12-16 16:18:53 -0800

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0
  */
+
 int main(void)
 {
 	unsigned long int i = 3, n = 612852475143;
-
+	
 	for (; i < 12057; i += 2)
 	{
 		while (n % i == 0 && n != i)
