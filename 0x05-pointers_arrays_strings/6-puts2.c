acebbc73d99d57712393400b94114b0767e634d4
villeantropist <villeantropist@gmail.com>
2022-12-19 17:30:19 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts2 - print alternating chars of string
  * @str: string
@@ -8,14 +7,15 @@
 
 void puts2(char *str)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
+
