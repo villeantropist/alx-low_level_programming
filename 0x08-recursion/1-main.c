ff4ad94bd5e57a57aa71220145bc6d92f8b450b5
villeantropist <villeantropist@gmail.com>
2023-01-04 21:06:33 +0000

    task 1

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-    _print_rev_recursion("\nColton Walker");
-    return (0);
+	_print_rev_recursion("\nColton Walker");
+	return (0);
 }
+
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-        _print_rev_recursion(s + 1);
-        _putchar(*s);
-    }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
+
