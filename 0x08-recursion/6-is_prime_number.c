86a7f367e72803a6697341a3903caa7bfe2c0860
villeantropist <villeantropist@gmail.com>
2023-01-04 21:29:36 +0000

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,19 +11,18 @@ int tmp_prime(int n, int i);
 
 int divisors(int n, int m)
 {
-    if (m % n == 0)
-    {
-        return (0);
-    }
-    else if (m / 2 > n)
-    {
-        return (divisors(n + 2, m));
-    }
-    else
-    {
-        return (1);
-    }
-}
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
 
 /**
  * is_prime_number - prime
@@ -33,12 +32,13 @@ int divisors(int n, int m)
 
 int is_prime_number(int n)
 {
-    if ((!(n % 2) && n != 2) || n < 2)
-    {
-        return (0);
-    }
-    else
-    {
-        return (divisors(3, n));
-    }
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
 }
+
