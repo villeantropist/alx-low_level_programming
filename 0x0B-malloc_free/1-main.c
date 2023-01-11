587ff9f65aed825863142f0738872291da90f508
villeantropist <villeantropist@gmail.com>
2023-01-11 00:09:30 +0000

    task 1

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -7,17 +7,19 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
+
