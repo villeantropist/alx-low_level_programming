a13fc6554ff95e8fc306a4b09d8a1336ac26ce6a
villeantropist <villeantropist@gmail.com>
2023-01-13 02:24:39 +0000

    task 1

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -7,12 +7,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *concat;
+	char *concat;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+	concat = string_nconcat("Best ", "School !!!", 6);
+	printf("%s\n", concat);
+	free(concat);
+	return (0);
 }
+
