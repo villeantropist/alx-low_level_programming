1f11f979990da5d1ac3552f8c9d01f7e61c6f120
villeantropist <villeantropist@gmail.com>
2023-01-18 23:52:00 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,18 +10,19 @@
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array && cmp)
-    {
-        for (i = 0; i < size; i++)
-        {
-            if (cmp(array[i]) != 0)
-                return (i);
-        }
-    }
-
-    return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
 }
+
--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -7,9 +7,10 @@
  *
  * Return: 0 if false, something else otherwise.
  */
+
 int is_98(int elem)
 {
-    return (98 == elem);
+	return (98 == elem);
 }
 
 /**
@@ -18,9 +19,10 @@ int is_98(int elem)
  *
  * Return: 0 if false, something else otherwise.
  */
+
 int is_strictly_positive(int elem)
 {
-    return (elem > 0);
+	return (elem > 0);
 }
 
 /**
@@ -29,9 +31,10 @@ int is_strictly_positive(int elem)
  *
  * Return: 0 if false, something else otherwise.
  */
+
 int abs_is_98(int elem)
 {
-    return (elem == 98 || -elem == 98);
+	return (elem == 98 || -elem == 98);
 }
 
 /**
@@ -39,16 +42,19 @@ int abs_is_98(int elem)
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, 
+		-98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int index;
 
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+	index = int_index(array, 20, is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, abs_is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, is_strictly_positive);
+	printf("%d\n", index);
+	return (0);
 }
+
