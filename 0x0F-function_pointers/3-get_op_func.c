e59f7119f7ccc7cc3c9460f79aba2b882286d934
villeantropist <villeantropist@gmail.com>
2023-01-19 00:07:00 +0000

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,25 +8,26 @@
  * Return: pointer to the function that corresponds to the
  * operator given as a parameter
  */
+
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}};
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while (ops[i].op)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
+
