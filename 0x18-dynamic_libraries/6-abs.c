a5cb9c2cc9661222c9f1a4a535474747642ce255
villeantropist <villeantropist@gmail.com>
2023-03-24 21:50:02 +0000

    update

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,15 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* _abs - prints the absolute value of a number
-*@i: number targeted
-*Return: returns 0
-*/
-int _abs(int i)
+ * _abs - value absolute
+ *
+ *@n: The int to print
+ * Return: Always 0.
+ */
+
+int _abs(int n)
 {
-if (i > 0)
-return (i);
-else if (i < 0)
-return (-i);
-else
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
 return (0);
 }
+
+else
+{
+return (n);
+}
+}
+
